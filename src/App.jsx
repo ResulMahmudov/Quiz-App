75f81e6db38f7538f7a44a933f4b8e0d895a414b
unknown <resul@gmail.com>
2025-09-05 00:39:45 +0400

    Quiz-app

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import TopicSelect from './Components/TopicSelect'
+
+const App = () => {
+  return (
+  <TopicSelect/>
+  )
+}
+
+export default App
---
+++ src/store/QuizStore.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const QuizStore = () => {
+  return (
+    <div>QuizStore</div>
+  )
+}
+
+export default QuizStore
